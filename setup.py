ca9b1fdfc454dc1f749c928676407e1dc2325dd6
Maarten <mviterson@gmail.com>
2022-01-08 10:10:41 +0100

    reorganise

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='file-differ',
+    description='check if two files are identical',
+    packages=find_packages(),
+    author='Maarten van Iterson',
+    entry_points="""
+    [console_scripts]
+    file-differ=filediffer:filediffer
+    """,
+    install_requires = ['click==8.0.3',
+                        'numpy==1.22.0',
+                        'pandas==1.3.5',
+                        'pylint==2.12.2',
+                        'pytest==6.2.5',
+                        'pytest-cov==3.0.0'],
+    version = '0.0.1',
+    url = 'https://github.com/mvaniterson/filediff.git'
+)
